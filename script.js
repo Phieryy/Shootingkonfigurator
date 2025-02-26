de9ec58dd427e7a6bfb9e268df6e4a3115b6ae8b
Phieryy <phillip@sven-seidler.de>
2025-02-26 14:18:01 +0100

    Update and rename Scriptkonfugurator.js to script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function selectOption(element) {
     // Entferne die "selected"-Klasse von allen Optionen
     const options = document.querySelectorAll('.option');
-    options.forEach(option => option.classList.remove('selected'));
-     // Füge die "selected"-Klasse der angeklickten Option hinzu
+    options.forEach(option => option.classList.remove('selected')); 
+
+    // Füge die "selected"-Klasse der angeklickten Option hinzu
     element.classList.add('selected');
 }
